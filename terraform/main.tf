df1934cc25959800422cbe32eb2e421407f9a8e2
andybig2022 <102986428+andybig2022@users.noreply.github.com>
2022-04-23 12:58:08 +0300

    Add files via upload

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,7 @@ provider "yandex" {
 }
 
 resource "yandex_compute_instance" "vm-1" {
-  name = "terraform1"
+  name = "host1"
 
   resources {
     cores  = 2
@@ -23,7 +23,7 @@ resource "yandex_compute_instance" "vm-1" {
 
   boot_disk {
     initialize_params {
-      image_id = "fd8oms2nh3a0k9bjekqd"
+      image_id = "fd879gb88170to70d38a"
     }
   }
 
@@ -35,8 +35,38 @@ resource "yandex_compute_instance" "vm-1" {
   metadata = {
     user-data = "${file("./meta.txt")}"
   }
-
+    scheduling_policy {
+    preemptible = true
+  }
 }
+
+ resource "yandex_compute_instance" "vm-2" {
+  name = "host2"
+
+  resources {
+    cores  = 2
+    memory = 2
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd879gb88170to70d38a"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    user-data = "${file("./meta.txt")}"
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+} 
+
 resource "yandex_vpc_network" "network-1" {
   name = "network1"
 }
@@ -54,4 +84,9 @@ output "internal_ip_address_vm_1" {
 output "external_ip_address_vm_1" {
   value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
 }
-
+output "internal_ip_address_vm_2" {
+  value = yandex_compute_instance.vm-2.network_interface.0.ip_address
+}
+output "external_ip_address_vm_2" {
+  value = yandex_compute_instance.vm-2.network_interface.0.nat_ip_address
+}
